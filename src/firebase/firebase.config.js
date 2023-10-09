bb596508de3dbd2fa290bccd9419e9a3512c1531
Jabed-Hasan <badboy07312@gmail.com>
2023-10-09 19:46:19 +0600

    Navbar Section added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAjfyB5en9Ny01SB846GVaRcT_U-DvpSuE",
+  authDomain: "event-management-79b6c.firebaseapp.com",
+  projectId: "event-management-79b6c",
+  storageBucket: "event-management-79b6c.appspot.com",
+  messagingSenderId: "817428966319",
+  appId: "1:817428966319:web:b5cab9017f9d14bfbb1d5a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/layout/Root.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from '../pages/Navbar/Navbar';
+import Login from '../pages/Login/Login';
+
+const Root = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+          
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default Root;
